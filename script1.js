c86af746a59a5d37ceb8b6be4e661341608c3e3c
Abhishek Kumar <abhishekupadhya19@gmail.com>
2023-10-20 20:15:29 +0530

    First push

---
+++ script1.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+let num1 = parseInt(prompt("Enter the first number:"));
+let num2 = parseInt(prompt("Enter the second number:"));
+let sum = num1 + num2;
+document.write(sum);
+console.log(sum);
+console.log("hello world");
+var firstName = "Sonu";
+var lastName = "Kumar";
+var fullName = firstName + "  " + lastName;
+console.log(firstName,lastName);
+
